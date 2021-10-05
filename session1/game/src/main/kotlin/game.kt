e000e73e028efdf9e8f9a5777ae88f5f9ef9d687
Somrad Sharma <somrad.sharma@student.ehb.be>
2021-10-05 12:28:04 +0200

    game
    game rock paper scissor

---
+++ session1/game/src/main/kotlin/game.kt
@@ -0,0 +1,31 @@
+val gameItems = arrayOf("rock", "paper", "scissor")
+val randomItem = gameItems.random()
+
+
+
+fun main() {
+
+    println(randomItem)
+
+    for (item in gameItems){
+
+
+        if(item == randomItem){
+
+        }
+        if (item == randomItem){
+
+        }
+        if (item == randomItem){
+
+        }
+
+    }
+
+
+    }
+
+
+
+
+
