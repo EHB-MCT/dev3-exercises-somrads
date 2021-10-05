598f6f9b8dbd521607f716573fd055317bacc29f
Somrad Sharma <somrad.sharma@student.ehb.be>
2021-10-05 11:35:27 +0200

    greeting mates
    greeting the mates

--- session1/src/main/kotlin/main.kt
+++ session1/src/main/kotlin/main.kt
@@ -1,3 +1,9 @@
-fun main () {
-    println("hello world")
+var classMates = arrayOf("Andy", "Alex", "Sabri", "Jianming", "Hajar")
+fun main() {
+   for (mate in classMates){
+       println("Hello my friend "  + mate);
+   }
 }
+
+
+
