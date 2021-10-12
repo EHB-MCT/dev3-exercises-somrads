e6200337a86cdeb7bc3d5d791d593b8aa7c11161
Somrad Sharma <somrad.sharma@student.ehb.be>
2021-10-12 12:34:55 +0200

    Update mygame.kt

--- session2/src/main/kotlin/mygame.kt
+++ session2/src/main/kotlin/mygame.kt
@@ -12,7 +12,7 @@ fun challengeOne() {
     if (playerAnwser != null) {
         if (playerAnwser.contains(correctAnswer)){
             println("You are correct, Monkey.D.Luffy is the name")
-            
+
         } else {
             println("Too bad " + playerAnwser + " is the wrong name " )
         }
