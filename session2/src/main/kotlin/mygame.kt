4814a89023252c189a6e25824a37b46024cc04de
Somrad Sharma <somrad.sharma@student.ehb.be>
2021-10-12 11:04:13 +0200

    challenge one gemaakt

--- session2/src/main/kotlin/mygame.kt
+++ session2/src/main/kotlin/mygame.kt
@@ -1,3 +1,17 @@
 fun main(){
-    println("hello");
+    println("Guess the name main character of One Piece is");
+    challengeOne()
+}
+
+fun challengeOne(){
+    val correctAnswer = "Luffy"
+    val playerAnwser = readLine();
+
+    if (playerAnwser != null) {
+        if (playerAnwser.contains(correctAnswer)){
+            println("You are correct, Monkey.D.Luffy is the name")
+        } else {
+            println("Too bad " + playerAnwser + " is the wrong name " )
+        }
+    }
 }
