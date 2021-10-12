1965d99aab12890d7f20d244fba1e95419b297f8
Somrad Sharma <somrad.sharma@student.ehb.be>
2021-10-12 12:35:46 +0200

    merge conflict fix

--- session2/src/main/kotlin/mygame.kt
+++ session2/src/main/kotlin/mygame.kt
@@ -1,29 +1,12 @@
-fun main(){
-    println("Guess the name main character of One Piece is");
-    challengeOne()
-
-}
-
-fun challengeOne(){
-    val correctAnswer = "Luffy"
-    val playerAnwser = readLine();
-
-    if (playerAnwser != null) {
-        if (playerAnwser.contains(correctAnswer)){
-            challengeTwo()
-        } else {
-            println("Too bad " + playerAnwser + " is the wrong name " )
-        }
-    }
-
-    fun challengeTwo(){
+ fun challengeTwo(){
         val userGuess = readLine().toString();
         val diceResult = (1..6).random();
 
         if (diceResult >=4){
-            winner()
+            println("win")
         } else {
-            gameOver()
+            println("loss")
+
         }
     }
 
