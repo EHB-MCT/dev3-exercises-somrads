014766e1a496ef2d686ecde817f191db8f93fb1d
Somrad Sharma <somrad.sharma@student.ehb.be>
2021-10-12 12:40:44 +0200

    Squashed commit of the following:
    commit 48ba111a4bd3d071616d8643459bf5525dff4473
    Author: Somrad Sharma <somrad.sharma@student.ehb.be>
    Date:   Tue Oct 12 12:39:30 2021 +0200
    
        Update mygame.kt
    
    commit 2d49af08f0d7bb1785ae467b00ab24e2de1a0268
    Author: Somrad Sharma <somrad.sharma@student.ehb.be>
    Date:   Tue Oct 12 12:38:05 2021 +0200
    
        merge conflict fix
    
    commit 1965d99aab12890d7f20d244fba1e95419b297f8
    Author: Somrad Sharma <somrad.sharma@student.ehb.be>
    Date:   Tue Oct 12 12:35:46 2021 +0200
    
        merge conflict fix
    
    commit 82c0928b042a1d1d8428b195a84d9a597604ee6a
    Author: Somrad Sharma <somrad.sharma@student.ehb.be>
    Date:   Tue Oct 12 12:27:55 2021 +0200
    
        file deleted
    
    commit 494372f2ac2d65ec0d8df6187ba96099aa1325e5
    Author: Somrad Sharma <somrad.sharma@student.ehb.be>
    Date:   Tue Oct 12 12:21:23 2021 +0200
    
        challenge two
    
        fix

--- session2/src/main/kotlin/mygame.kt
+++ session2/src/main/kotlin/mygame.kt
@@ -1,5 +1,33 @@
+fun main(){
+    println("play the game");
+    challengeOne();
+}
 
+fun challengeOne() {
+    val correctAnswer = "Luffy"
+    val playerAnwser = readLine();
 
+    if (playerAnwser != null) {
+        if (playerAnwser.contains(correctAnswer)){
+            println("You are correct, Monkey.D.Luffy is the name")
 
+        } else {
+            println("Too bad " + playerAnwser + " is the wrong name " )
+        }
+    }
+
+}
+
+fun challengeTwo(){
+        val userGuess = readLine().toString();
+        val diceResult = (1..6).random();
+
+        if (diceResult >=4){
+            println("win")
+        } else {
+            println("loss")
+
+        }
+    }
 
 
