ffc45093f71bec7866bea40f67b3db212e1cf25e
Somrad Sharma <somrad.sharma@student.ehb.be>
2021-10-12 12:38:30 +0200

    fix

--- session2/src/main/kotlin/mygame.kt
+++ session2/src/main/kotlin/mygame.kt
@@ -1,25 +1,3 @@
-fun main(){
-    println("play the game");
-    challengeOne();
-
-
-}
-
-fun challengeOne() {
-    val correctAnswer = "Luffy"
-    val playerAnwser = readLine();
-
-    if (playerAnwser != null) {
-        if (playerAnwser.contains(correctAnswer)){
-            println("You are correct, Monkey.D.Luffy is the name")
-
-        } else {
-            println("Too bad " + playerAnwser + " is the wrong name " )
-        }
-    }
-
-}
-
 
 
 
