9e84fce30f203903964a5f8124f9a68cb68bc49f
Somrad Sharma <somrad.sharma@student.ehb.be>
2021-10-12 11:43:31 +0200

    aanpassingen aan challenge 2

--- session2/src/main/kotlin/challengeTwo.kt
+++ session2/src/main/kotlin/challengeTwo.kt
@@ -9,12 +9,11 @@ val diceNumbers =  intArrayOf(1, 2, 3,4,5,6).random()
 fun challengeTwo(){
     val correctAnswer = "Luffy"
     val playerAnwser = readLine();
-    
+
     }
 
     fun generateDice(){
         if (diceNumbers <=3){
-            diceNumbers.pushTo
         }
     }
 
